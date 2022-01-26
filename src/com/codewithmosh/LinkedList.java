47981af4322a24fdd66c1df7e552d25059e796e1
marcozhou <marcoonway@gmail.com>
2022-01-26 20:49:06 +0800

    ..

--- src/com/codewithmosh/LinkedList.java
+++ src/com/codewithmosh/LinkedList.java
@@ -1,31 +1,152 @@
 package com.codewithmosh;
 
+
+import java.util.NoSuchElementException;
+
 public class LinkedList {
-    private Node first;
-    private Node last;
+    public class Node {
+        private int value;
+        private Node next;
 
-    public void addFirst(Node node) {
-        first.updateNext(node);
-    }
-
-    public void addLast(Node node) {
-        last.updateNext(node);
-    }
-
-    public void deleteFirst() {
-        first.clearNext();
-    }
-
-    public void deleteLast() {
-        last.clearNext();
-    }
-
-    public boolean contains(Node node) {
-        if (first.getValue() == node.getValue()) {
-            return true;
-        } else {
-
+        public Node(int item) {
+            this.value = item;
         }
     }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void addLast(int item) {
+        var node = new Node(item);
+        if (first == null) first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+
+        size++;
+    }
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+        if (first == null) first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+
+        size++;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        Node current = first;
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+
+        return indexOf(item) != -1;
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        if (first == last)
+            first = last = null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+
+        size--;
+    }
+
+
+    public void removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+        if (first == last)
+            first = last = null;
+        else {
+            var previous = getPrevious(last);
+            last = previous;
+            previous.next = null;
+        }
+
+        size--;
+    }
+
+    public Node getPrevious(Node last) {
+        var current = first;
+        while (current != null) {
+            if (current.next == last) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int[] toArray() {
+        var array = new int[size];
+        var index = 0;
+        var current = first;
+        while (current != null) {
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    public void reverse() {
+//      first                   last
+//      [10 <- 20  <- 30  40 -> 50]
+        if (isEmpty()) return;
+
+        var previous = first;
+        var current = first.next;
+
+        while (current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+    }
+
+    // [1 -> 2 -> 3 -> 4 -> 5]
+    //                 k
+    //                 size-k
+    public int findKthFromTheEnd(int k) {
+        Node curr = first;
+
+
+        if (k > size) return 0;
+
+
+        for (int i = 1; i < size - k + 1; i++) {
+            curr = curr.next;
+        }
+
+        return curr.value;
+    }
+
 
 }
--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,18 +1,28 @@
 package com.codewithmosh;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+
 
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
+
+
+
         list.addLast(10);
-        list.addLast(120);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        list.addLast(60);
+        list.addLast(70);
 
-        System.out.println(list.contains(10));
-        System.out.println(Arrays.toString(list.toArray()));
+        var res = list.findKthFromTheEnd(4);
 
+        System.out.println(res);
 
-
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
