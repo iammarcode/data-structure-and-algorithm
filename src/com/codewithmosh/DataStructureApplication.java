c6f043ec0dff2569b835390303ea0c8313fe42b6
marcozhou <marcoonway@gmail.com>
2022-02-20 01:48:56 +0800

    min stack

--- src/com/codewithmosh/DataStructureApplication.java
+++ src/com/codewithmosh/DataStructureApplication.java
@@ -2,6 +2,8 @@ package com.codewithmosh;
 
 import com.codewithmosh.stack.ArrayStack;
 import com.codewithmosh.stack.Expression;
+import com.codewithmosh.stack.MinStack1;
+import com.codewithmosh.stack.MinStack2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,16 +12,27 @@ import java.util.Stack;
 
 public class DataStructureApplication {
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack();
+        var stack = new MinStack2();
 
         stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
+        stack.push(2);
+        stack.push(56);
+        stack.push(33);
+        stack.push(1);
 
-//        stack.pop();
+        System.out.println(stack.getMin());
 
-        System.out.println(stack.peek());
+        stack.push(100);
+
+        System.out.println(stack.getMin());
+
+        stack.pop();
+        stack.pop();
+
+        System.out.println(stack.getMin());
+
+
+        System.out.println(stack.top());
 
 
     }
---
+++ src/com/codewithmosh/stack/MinStack1.java
@@ -0,0 +1,34 @@
+package com.codewithmosh.stack;
+
+// array solution
+public class MinStack1 {
+    private int count;
+    private int[] items, minTrack;
+
+    public MinStack1(int len) {
+        items = new int[len];
+        minTrack = new int[len];
+    }
+
+    public void push(int item) {
+        if (count == 0) {
+            minTrack[count] = 0;
+        } else {
+            if (items[minTrack[count - 1]] > item) {
+                minTrack[count] = count;
+            } else {
+                minTrack[count] = minTrack[count - 1];
+            }
+        }
+        items[count++] = item;
+    }
+
+    public int pop() {
+        if (count == 0) throw new IllegalStateException();
+        return items[--count];
+    }
+
+    public int min() {
+        return items[minTrack[count - 1]];
+    }
+}
