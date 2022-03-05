637692893b98956575674d46678369619a67d817
marcozhou <marcoonway@gmail.com>
2022-03-06 00:18:18 +0800

    reverse first k elements of queue

--- src/com/codewithmosh/DataStructureApplication.java
+++ src/com/codewithmosh/DataStructureApplication.java
@@ -1,6 +1,7 @@
 package com.codewithmosh;
 
 import com.codewithmosh.queue.PriorityQueue;
+import com.codewithmosh.queue.QueueReverser;
 
 import java.util.*;
 
@@ -8,14 +9,16 @@ import java.util.*;
 public class DataStructureApplication {
     public static void main(String[] args) {
 
-        PriorityQueue queue = new PriorityQueue();
+        QueueReverser queue = new QueueReverser(4);
 
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
         queue.enqueue(4);
         queue.enqueue(5);
-        queue.enqueue(3);
         queue.enqueue(6);
-        queue.enqueue(2);
-//        queue.enqueue(4);
+
+        queue.reverse(5);
 
         System.out.println(queue);
 
---
+++ src/com/codewithmosh/queue/QueueReverser.java
@@ -0,0 +1,51 @@
+package com.codewithmosh.queue;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class QueueReverser {
+    private int front, rear, count;
+    private int[] items;
+
+    public QueueReverser(int capacity) {
+        items = new int[capacity];
+    }
+
+    public void enqueue(int item) {
+        if (count >= items.length) {
+            resize();
+        }
+
+        items[count++] = item;
+    }
+
+    public void resize() {
+        int[] newItems = new int[items.length * 2];
+        for (int i = 0; i < items.length; i++) {
+            newItems[i] = items[i];
+        }
+        items = newItems;
+    }
+
+    public void reverse(int k) {
+        if (k > count) throw new IllegalStateException();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < k; i++) {
+            stack.push(items[i]);
+        }
+
+        for (int i = 0; i < k; i++) {
+            items[i] = stack.pop();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "QueueReverser{" +
+                "front=" + front +
+                ", rear=" + rear +
+                ", count=" + count +
+                ", items=" + Arrays.toString(items) +
+                '}';
+    }
+}
